e6b08e74823c8ce94efc0708ae8d2332ecbe17ce
Revathy Jalaja Kumari <rjalajak@my.centennialcollege.ca>
2023-03-01 12:02:52 -0500

    Add Book page created

---
+++ .history/server/config/config_20230301084342.js
@@ -0,0 +1,2 @@
+export const MongoURI = "mongodb+srv://v7:snowflake1@cluster0.87a3ja9.mongodb.net/media?retryWrites=true&w=majority"
+export const Secret = "someSecret";
---
+++ .history/server/config/config_20230301103753.js
@@ -0,0 +1,2 @@
+/*export const MongoURI = "mongodb+srv://v7:snowflake1@comp229.mi1r7z1.mongodb.net/media?retryWrites=true&w=majority"
+export const Secret = "someSecret";*/
